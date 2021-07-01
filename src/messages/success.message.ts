69e0b394acb80a39e89ca17e68b4d349853a3a2b
nkopak <123nazik@gmail.com>
2021-07-02 02:02:02 +0300

    status codes, success msg

---
+++ src/messages/success.message.ts
@@ -0,0 +1,9 @@
+export const successMessage = {
+  TODO_ITEM_CREATED: 'Todo item was successfully created.',
+  TODO_ITEM_UPDATED: 'Todo item was successfully updated.',
+  TODO_ITEM_DELETED: 'Todo item was successfully deleted.',
+
+  TODO_LIST_CREATED: 'List was successfully created.',
+  TODO_LIST_UPDATED: 'List was successfully updated.',
+  TODO_LIST_DELETED: 'List was successfully deleted.'
+};
