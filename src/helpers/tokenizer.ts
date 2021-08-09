14f5980067f0a1d58e0555d8d558674f60d712b7
nkopak <123nazik@gmail.com>
2021-08-09 16:13:34 +0300

    minor tokenizer change

--- src/helpers/tokenizer.ts
+++ src/helpers/tokenizer.ts
@@ -8,7 +8,7 @@ export const tokenizer = (
   access_token: string;
   refresh_token: string;
 } => {
-  const payload = { id, role };
+  const payload = { id, firstName, role };
 
   const access_token = jwt.sign(payload, 'JWT_SECRET', { expiresIn: '1d' });
   const refresh_token = jwt.sign({}, 'JWT_REFRESH', { expiresIn: '30d' });
